a7bf490bfa80207561af221128c6123dc7931249
emielster <emielster.dev@gmail.com>
2026-02-01 22:50:19 +0100

    Fixed an error

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { SpaceScene } from './components/SpaceScene';
 import { Navigation } from './components/Navigation';
 import { Hero } from './components/Hero';
 import { Projects } from './components/Projects';
-import { About } from './components/About';
 import { Contact } from './components/Contact';
 import { Skills } from './components/Skills';
 import './index.css';
